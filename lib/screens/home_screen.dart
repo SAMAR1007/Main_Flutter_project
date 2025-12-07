1d4897cb60a8eaca1a2996f851472ecf45bef3c6
SAMAR1007 <bistsamar999@gmail.com>
2025-12-07 22:00:14 +0545

    home screen completed

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+// lib/screens/home_screen.dart
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('This is home screen'),
+      ),
+    );
+  }
+}
