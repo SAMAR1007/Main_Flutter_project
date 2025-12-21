070055c1e010d19e0069bd5577d775c65ed3ef89
SAMAR1007 <bistsamar999@gmail.com>
2025-12-21 23:07:51 +0545

    cart page completed

---
+++ lib/screens/bottom_screen/cart_page.dart
@@ -0,0 +1,18 @@
+// lib/screens/cart_page.dart
+import 'package:flutter/material.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Cart Page',
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
