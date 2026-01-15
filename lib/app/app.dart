82d0083e316f3be15cc68571f134064a7b77dbdd
SAMAR1007 <bistsamar999@gmail.com>
2026-01-15 20:42:03 +0545

    app.dart completed

---
+++ lib/app/app.dart
@@ -0,0 +1,20 @@
+// lib/app/app.dart
+
+import 'package:flutter/material.dart';
+import 'routes/app_routes.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tech Hive',
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.splash,
+      routes: {
+        AppRoutes.splash: (context) => const SizedBox.shrink(),
+      },
+    );
+  }
+}
