60e0992258e1ecc1d95f40859b742da17b578fc6
SAMAR1007 <bistsamar999@gmail.com>
2026-01-15 20:36:53 +0545

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'screens/splash_screen.dart';
+import 'data/models/user.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserAdapter());
+  await Hive.openBox<User>('users');
   runApp(const TechHiveApp());
 }
 
