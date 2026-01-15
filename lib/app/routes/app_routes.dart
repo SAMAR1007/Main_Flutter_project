898563b7b461ed1a58caebd173a79c96116a3c08
SAMAR1007 <bistsamar999@gmail.com>
2026-01-15 20:41:06 +0545

    app routes created

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,9 @@
+// lib/app/routes/app_routes.dart
+
+class AppRoutes {
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+}
