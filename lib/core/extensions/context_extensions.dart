b51a7160ef7b4ad675f547d083b9d09b1c94be96
SAMAR1007 <bistsamar999@gmail.com>
2026-03-08 20:30:27 +0545

    Add BuildContext extension methods

---
+++ lib/core/extensions/context_extensions.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+/// Extension methods on [BuildContext] for cleaner access to common properties.
+extension BuildContextExtensions on BuildContext {
+  ThemeData get theme => Theme.of(this);
+  TextTheme get textTheme => Theme.of(this).textTheme;
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+  double get screenWidth => MediaQuery.of(this).size.width;
+  double get screenHeight => MediaQuery.of(this).size.height;
+  EdgeInsets get padding => MediaQuery.of(this).padding;
+
+  void showSnackBar(String message, {bool isError = false}) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: isError ? Colors.red.shade700 : null,
+        behavior: SnackBarBehavior.floating,
+      ),
+    );
+  }
+}
